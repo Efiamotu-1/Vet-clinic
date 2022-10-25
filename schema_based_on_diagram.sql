148fc119ca604e73084f9ed14b81207a0477f55c
yosefHesham <youssef.h@trufla.com>
2022-10-25 22:00:03 +0200

    Create join and foriegn keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,13 +9,13 @@ CREATE TABLE invoices (
     total_amount DECIMAL,
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
-    medical_history_id INTEGER
+    medical_history_id INTEGER references medical_histories(id)
 );
 
 CREATE TABLE medical_histories (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     admitted_at TIMESTAMP,
-    patient_id INTEGER,
+    patient_id INTEGER references patients(id),
     status VARCHAR
 );
 
@@ -24,8 +24,8 @@ CREATE TABLE invoice_items (
     unit_price DECIMAL,
     quantity INTEGER,
     total_price DECIMAL,
-    invoice_id INTEGER,
-    treatment_id INTEGER
+    invoice_id INTEGER references invoices(id),
+    treatment_id INTEGER references treatments(id)
 );
 
 CREATE TABLE treatments (
@@ -33,3 +33,10 @@ CREATE TABLE treatments (
     type VARCHAR,
     name VARCHAR
 );
+
+
+
+CREATE TABLE medical_histories_treatments (
+    medical_history_id int references medical_histories(id);
+    treatment_id int references treatments(id)
+)
